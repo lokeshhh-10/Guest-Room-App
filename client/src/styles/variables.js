c3ba2c7fcfd946bc2a2812695122e4ca98b43604
lokesh <lokesh95664@gmail.com>
2024-07-26 23:09:11 +0530

    ......

---
+++ client/src/styles/variables.js
@@ -0,0 +1,11 @@
+const variables = {
+  pinkred: "#F8395A",
+  violet : '#8371fd',
+  tangerine : '#ff8c6b',
+  blue: "#24355A",
+  lightgrey: "#F7F8F8",
+  grey: "#bdb9b9",
+  darkgrey: "#969393",
+};
+
+export default variables;
